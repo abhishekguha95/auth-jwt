2aae4f83343c1697bf67e601530ffafda1f928cb
Abhishek Guha <qohor2525@gmail.com>
2021-08-26 19:28:19 +0530

    commit2

--- server.js
+++ server.js
@@ -4,17 +4,14 @@ const https = require("https");
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => {      // home page route
   res.json({
     message: "Welcome page",
   });
 });
 
-// setTimeout(function func() {
-//   console.log("2", user_info);
-// }, 3000);
 
-app.get("/auth/google", (req, res) => {
+app.get("/auth/google", (req, res) => {      // auth route 
 
   let user_info;
   let options = {
@@ -22,17 +19,17 @@ app.get("/auth/google", (req, res) => {
     hostname: "www.googleapis.com",
     path: "/userinfo/v2/me",
     headers: {
-      Authorization:
-        "Bearer ya29.a0ARrdaM-PKXYfKvPfYn0x-E3M0MLukWH94RJWccQPe5a80kySFKjrfJZgruJJfaEoMpd0rgHg_pD4WZxG_vISlcbU1xTmndJY7qY9JF_-kBCHf99GEcUDb0H20qh-LVutkAIhqVd-JlqxIJe5a_H3cCRk_HKE",
+      Authorization:  // bearer token taken from Google Auth playground,needs to be updated after it is expired
+        "Bearer ya29.a0ARrdaM9ObhfH0-8MnNmwBrCzBH8ZrOeaMw1HCcrY7H-7sd9RgycenrlaoZLmbzYxUsXmyF6Vj0TrG0sa3MCHELLyvSa1bDBUIbTpo3cRZ8axayGPBkUnCxoK7mZYeymcxci8_m5lZDj9GaWAjfEOsCdAhFUt",
     },
   };
   
   let req2 = https.request(options, (res2) => {
     res2
       .on("data", (chunk) => {
-        user_info = JSON.parse(chunk);
+        user_info = JSON.parse(chunk);    //response is a buffer, converted to json
         console.log("1", user_info);
-        res.json({'email':user_info.email, 'name': user_info.name})
+        res.json({'email':user_info.email, 'name': user_info.name})     // final response of email and name
       })
       .on("error", function (error) {
         console.error(error);
